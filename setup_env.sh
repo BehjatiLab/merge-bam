e5da49adcf451fff14974bb5855bd5e27e94e102
Lorcan <lorcanpd@gmail.com>
2024-09-11 13:46:16 +0100

    Package installing script now exits with status 1 if any stage doesn't complete successfully.

--- setup_env.sh
+++ setup_env.sh
@@ -11,15 +11,35 @@ REPO_URL="https://github.com/BehjatiLab/merge-bam"
 echo "Creating environment '$ENV_NAME' at $ENV_PATH."
 conda create -p "$ENV_PATH" python=3.11.9 -y
 
+if [ $? -ne 0 ]; then
+  echo "Failed to create the environment. Exiting."
+  exit 1
+fi
+
 # Activate the newly created environment
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate "$ENV_PATH"
+if [ $? -ne 0 ]; then
+  echo "Failed to activate the environment. Exiting."
+  exit 1
+fi
 
 # Install pip in the conda environment
 conda install pip -y
 
+if [ $? -ne 0 ]; then
+  echo "Failed to install pip. Exiting."
+  exit 1
+fi
+
 # Clone the GitHub repository
 git clone "$REPO_URL"
+
+if [ $? -ne 0 ]; then
+  echo "Failed to clone the repository. Exiting."
+  exit 1
+fi
+
 REPO_DIR=$(basename "$REPO_URL" .git)
 
 # Navigate to the cloned directory
@@ -27,6 +47,11 @@ cd "$REPO_DIR" || exit
 
 # Install the package from the cloned repository
 pip install .
+
+if [ $? -ne 0 ]; then
+  echo "Failed to install the package. Exiting."
+  exit 1
+fi
 
 # Navigate back to the parent directory and remove the cloned repository
 cd ..
